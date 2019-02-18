a69e4d6d73f1fc62011ac1f25f1bf7025d086c25
donsizemore <don.sizemore@gmail.com>
2019-02-18 10:44:30 -0500

    creates datasets properly

--- tests/sampledata/users/create-users.sh
+++ tests/sampledata/users/create-users.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 SERVER='http://localhost:8080/api'
 BURRITO='burrito'
-USERDIR='/vagrant/tests/sampledata/users'
 
-for i in `cd $USERDIR && ls -1 *.user`; do
+for i in `cd $1 && ls -1 *.user`; do
   echo $i
-  pass=`cat $USERDIR/$i | jq .userName | tr -d \"`
+  pass=`cat $i | jq .userName | tr -d \"`
   echo $pass
-  resp=$(curl -s -H "Content-type:application/json" -X POST -d @$USERDIR/$i "$SERVER/builtin-users?password=$pass&key=$BURRITO")
-  mkdir -p $USERDIR/out
+  resp=$(curl -s -H "Content-type:application/json" -X POST -d @$i "$SERVER/builtin-users?password=$pass&key=$BURRITO")
+  mkdir -p out
   echo $resp
-  echo $resp | jq . > $USERDIR/out/$i
+  echo $resp | jq . > out/$i
 done
