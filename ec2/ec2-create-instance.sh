fee6a4ec8cf3d9b96c33815529c3fc3847fab359
don.sizemore <don.sizemore@github.com>
2020-04-22 12:31:31 -0400

    #140 use current ansible, shorten SSH command argument so it doesn't get truncated

--- ec2/ec2-create-instance.sh
+++ ec2/ec2-create-instance.sh
@@ -169,8 +169,7 @@ fi
 # TODO: Add some error checking for this ssh command.
 ssh -T -i $PEM_FILE -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile=/dev/null' -o 'ConnectTimeout=300' $USER_AT_HOST <<EOF
 sudo yum -y install epel-release
-sudo yum -y install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.7.9-1.el7.ans.noarch.rpm
-sudo yum -y install git nano
+sudo yum -y install ansible git nano
 git clone -b $DA_BRANCH https://github.com/IQSS/dataverse-ansible.git dataverse
 export ANSIBLE_ROLES_PATH=.
 ansible-playbook -v -i dataverse/inventory dataverse/dataverse.pb --connection=local $GVARG
