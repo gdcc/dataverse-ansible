0084e99f1d2710fb0950e9c4de34c7b633ba7dd3
don.sizemore <don.sizemore@github.com>
2021-11-03 12:39:17 -0400

    #179 enable postgres statement logging during integration tests

---
+++ files/count.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+my $pglogfile = shift @ARGV;
+
+unless ( -f $pglogfile )
+{
+    die "usage: ./count.pl <PGLOGFILE>\n";
+}
+
+my $pglogfilesize = (stat($pglogfile))[7];
+print "Current size: ".$pglogfilesize." bytes.\n";
+
+system "./parse.pl < $pglogfile > tail.parsed";
+
+system "cat tail.parsed | sed 's/ where.*//' | sed 's/ WHERE.*//' | sort | uniq -c | sort -nr -k 1,2 > tail.counted";
+
+print "Parsed and counted the queries. Total number:\n";
+
+system "awk '{a+=\$1}END{print a}' < tail.counted";
+
+print "\nQueries, counted and sorted: \n\n";
+
+system "cat tail.counted";
